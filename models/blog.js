3e2b08371bddb089a4e806dd41bdff2644583417
karankataria77 <karankataria771801@gmail.com>
2020-10-16 21:15:41 +0530

    all file added

---
+++ models/blog.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    blog: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
+})
+
+module.exports = mongoose.model("Blog", blogSchema)
---
+++ views/main/api.js
@@ -0,0 +1,4 @@
+const clientId = "248573100003-jg8req5ueqvgmqv6ib1aornommbv6e16.apps.googleusercontent.com"
+
+const clientSecret = "uKKCAScF1SOx7WGf1Sjs2_Si"
+
